bc5deced24b44f04ce7fda94882ed40ba9607b85
chaoting <02115214@gm.scu.edu.tw>
2021-02-22 18:54:13 +0800

    fix totopbutton rwd

--- src/Components/Share/ToTopButton.js
+++ src/Components/Share/ToTopButton.js
@@ -30,7 +30,7 @@ const ToTopButtonWrap = styled.span`
     width: 37px;
     margin-right: 1px;
   }
-  @media only screen and (max-width: 500px) {
+  @media only screen and (max-width: 900px) {
     display: none;
   }
 `;
